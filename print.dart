3b86e34ad8329c20ca8dffac3a76a5f87d5eda9d
Joaquim Köster <joaquimkoster@gmail.com>
2026-03-24 08:27:49 -0300

    Add files via upload

---
+++ print.dart
@@ -0,0 +1,3 @@
+void main() {                                                                   // Mandatory entry point for any Dart program
+  print('Olá, Joaquim!');                                                       // Built-in function to display text in the console
+}                                                                               // End of the main function scope
